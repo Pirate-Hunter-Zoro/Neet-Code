d21be302d18d28ffb7fba0e42b6b9634cdd7846c
Mikey Ferguson <mikeytallyferguson@gmail.com>
2024-11-21 13:29:38 -0600

    segment tree

---
+++ data_structures/segment_tree.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class SegmentTree:
+    
+    def __init__(self, nums: List[int]):
+        pass
+    
+    def update(self, index: int, val: int) -> None:
+        pass
+    
+    def query(self, L: int, R: int) -> int:
+        pass
