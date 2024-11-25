724b370d99a98c775259f6aa3e58fa1bea012be5
Mikey Ferguson <mikeytallyferguson@gmail.com>
2024-11-25 15:35:50 -0600

    segment_tree

--- all_test.py
+++ all_test.py
@@ -3,6 +3,23 @@ import unittest
 
 from solution import Solution
 
+from typing import List
+import unittest
+
+from data_structures.segment_tree import SegmentTree
+
+class SegmentTreeTest(unittest.TestCase):
+    
+    
+    def test_segment_tree(self):
+        seg = SegmentTree(nums=[1, 2, 3, 4, 5])
+
+        self.assertTrue(seg.query(L=0, R=2) == 6)
+        self.assertTrue(seg.query(L=2, R=4) == 12)
+        seg.update(index=3, val=0)
+        self.assertTrue(seg.query(L=2, R=4) == 8)
+
+
 class SolutionTest(unittest.TestCase):
     
     
