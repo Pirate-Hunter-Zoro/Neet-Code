bac32d4799d1a3d74f7ad5c1ab79577bfe150376
Mikey Ferguson <mikeytallyferguson@gmail.com>
2024-11-30 16:18:39 -0600

    some progress

--- data_structures/tree_node.py
+++ data_structures/tree_node.py
@@ -5,6 +5,8 @@ class TreeNode:
     def __init__(self, val=0, left=None, right=None, values: List[int]=None):
         if values != None:
             # One way to initialize a tree node
+            self.val = values[0]
+            current_level = [self]
             pass
         else:
             # Other way to initialize a tree node
